209f5a85e676f54295057bb685a0b0aef515f0f2
Rahul Vishwakarma <champ0843@gmail.com>
2020-03-01 13:27:12 +0530

    cricbuzz quote qork

--- quotesbot/spiders/toscrape-css.py
+++ quotesbot/spiders/toscrape-css.py
@@ -1,22 +1,48 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import pandas as pd
 
+cric_url = "https://www.cricbuzz.com/profiles/25/sachin-tendulkar"
+cric_url = "https://www.cricbuzz.com/profiles/1413/virat-kohli"
+cric_url = "https://www.cricbuzz.com/profiles/1447/ajinkya-rahane"
+cric_url = "https://www.cricbuzz.com/profiles/8733/lokesh-rahul"
+#cric_url = "http://quotes.toscrape.com/"
+#//*[@id="playerProfile"]/div[2]/div[2]/div/div[2]/table
+def create_table(xpath,response):
+    column = []
+    data = []
+    for quote in response.selector.xpath(xpath):
+        for thead in quote.css("thead"):
+            for trow in thead.css("tr"):
+                column = trow.css("th::attr(title)").extract()
+                column = ['Format'] + column
+        for tbody in quote.css("tbody"):
+            for trow in tbody.css("tr"):
+                print(trow)
+                print(trow.css("td > strong::text").extract())
+                mid_dat = trow.css("td > strong::text").extract() + trow.css("td::text").extract()
+                print(mid_dat)
+                data.append(mid_dat)
+    return pd.DataFrame(data = data,columns = column)
 
 class ToScrapeCSSSpider(scrapy.Spider):
     name = "toscrape-css"
     start_urls = [
-        'http://quotes.toscrape.com/',
+        cric_url,
     ]
 
-    def parse(self, response):
-        for quote in response.css("div.quote"):
-            yield {
-                'text': quote.css("span.text::text").extract_first(),
-                'author': quote.css("small.author::text").extract_first(),
-                'tags': quote.css("div.tags > a.tag::text").extract()
-            }
+    record = pd.DataFrame()
 
-        next_page_url = response.css("li.next > a::attr(href)").extract_first()
-        if next_page_url is not None:
-            yield scrapy.Request(response.urljoin(next_page_url))
+    def parse(self, response):
+        column = []
+        data = []
+        bat_record = create_table('//*[@id="playerProfile"]/div[2]/div[2]/div/div[2]/table',response)
+        bowl_record = create_table('//*[@id="playerProfile"]/div[2]/div[2]/div/div[3]/table',response)
+        print(data)
+        print(column)
+        print(bat_record)
+        print(bowl_record)
+        #next_page_url = response.css("li.next > a::attr(href)").extract_first()
+        #if next_page_url is not None:
+        #    yield scrapy.Request(response.urljoin(next_page_url))
 
